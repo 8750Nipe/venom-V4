ff76cf88425ca0eafd58269fb712d841d2981ef9
Golu8750 <nipendrakumar9717@gmail.com>
2024-07-18 22:50:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7260159150:AAG-D0kQ526w5Ka4r6c8EoOMqn8npsFJSB8")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("@Golugaming_vvipbot")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6795450311")
 
 
 
